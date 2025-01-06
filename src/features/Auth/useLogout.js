c3c75d73e8d5c22159185087e063ffc41effd9dd
hatran216004 <qiqisokiu.321@gmail.com>
2025-01-06 15:23:58 +0700

    navigate khi logout

--- src/features/Auth/useLogout.js
+++ src/features/Auth/useLogout.js
@@ -2,8 +2,10 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { logout as logoutApi } from '../../services/apiAuth';
 import toast from 'react-hot-toast';
 import { useAppContext } from '../../context/AppContext';
+import { useNavigate } from 'react-router-dom';
 
 export function useLogout() {
+  const navigate = useNavigate();
   const queryClient = useQueryClient();
   const { setIsAuthenticated } = useAppContext();
   const { mutate: logout, isLoading } = useMutation({
@@ -13,6 +15,7 @@ export function useLogout() {
       queryClient.removeQueries();
       setIsAuthenticated(false);
       toast.success('Logout successfully');
+      navigate('/');
     }
   });
 
